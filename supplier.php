f357fe7823991db649b024d9cf0b47b7399ec312
明洲 李 <mingzhouli@mingzhouli.local>
2017-06-22 10:21:52 +1000

    suppliers page

---
+++ supplier.php
@@ -0,0 +1,115 @@
+<style>
+.suppliers-div{
+	background-color: #EFEFEF; 
+	color:#4A4A4A;
+	padding-top:30px;
+	/*text-align: center;*/
+}
+.suppliers-div h2{
+	font-family: Montserrat-Bold;
+	font-size: 24px;
+	text-align: center;
+}
+#suppliers-wrapper{
+	margin:0 auto;
+	width: 600px;
+	margin-top:30px;
+	text-align: center;
+}
+.suppliers-left{
+	width: 340px;
+	float:left;
+	border-right: 2px solid #4ABDAC;
+}
+.suppliers-right{
+	width: 221px;
+	float:left;
+	text-align: center;
+	padding-left:35px;
+}
+.suppliers-title{
+	font-family: Montserrat;
+	font-size: 14px;
+	border-bottom: 2px solid #4ABDAC;
+	height: 28px;
+	width:305px;
+	text-align: left;
+}
+.suppliers-des{
+	width:285px;
+	padding-left:20px;
+	/*border-bottom: 2px solid #4ABDAC;*/
+	/*height: 28px;*/
+	text-align: left;
+}
+.suppliers-des li{
+	padding:10px 0;
+}
+.suppliers-right img{
+	margin-top:58px;
+	margin-bottom:30px;
+}
+.suppliers-des,
+.suppliers-right p{
+	font-family: Montserrat-Light;
+	font-size:12px;
+}
+.suppliers-right>p>a{
+	color: #4ABDAC;
+	text-decoration: none;
+}	
+.suppliers-right>a{
+	text-decoration: none;
+}
+.suppliers-right>a>div{
+	text-decoration: none;
+	width:114px;
+	height:40px;
+	color:#4ABDAC;
+	border:2px solid #4ABDAC;
+	font-family: Montserrat-Bold;
+	font-size: 16px;
+	line-height: 40px;
+	margin:95px auto 0 auto;
+	transition: all .3s linear;
+}
+.suppliers-right>a>div:hover{
+	color:#FFF;
+	background-color: #4ABDAC;
+}
+#suppliers-bottom{
+	margin-top:90px;
+}
+</style>
+<div class="suppliers-div">
+	<h2>
+		SUPPLIERS
+	</h2>
+	<div id="suppliers-wrapper">
+		<div class="suppliers-left">
+			<div class="suppliers-title">New Global Requests</div>
+			<ul class="suppliers-des">
+				<li>All products have to comply with Australian Standards and Regulations.</li>
+				<li>All products need to be safe to use.</li>
+				<li>All products must comply with product labeling, barcode format and shipping marking standards.</li>
+			</ul>
+			<div class="suppliers-title">As Our Supplier</div>
+			<ul class="suppliers-des">
+				<li>Keep our confidential information. </li>
+				<li>Competitive in pricing.</li>
+				<li>Be consistent in quality, trustworthy and cooperative in practice.</li>
+				<li>Comply with Australian Law and local law and regulation.</li>
+			</ul>
+		</div>
+	
+		<div class="suppliers-right">
+			<img src='images/new/supplier-email.png'/>
+			<p>
+			If you consider you are the right one, please email me your submission to 
+				<a href="mailto:buyer@newglobalmel.com.au" class="hvr-underline-from-left-blue">buyer@newglobalmel.com.au</a>
+			</p>
+			<a href='/'><div>Contact Us</div></a>
+		</div>
+		<img id="suppliers-bottom" src='images/new/supplier-bottom.png'/>
+	</div>
+</div>
